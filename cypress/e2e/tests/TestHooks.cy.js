6afc1f4cbdb4ea0bc301eeb84d59337f412166d0
jyotijaya <prakash.mishra201717@gmail.com>
2023-12-28 23:32:41 +0530

    Commit test hooks

---
+++ cypress/e2e/tests/TestHooks.cy.js
@@ -0,0 +1,15 @@
+///<reference types="Cypress"/>
+import { Register } from "../pages/register";
+const register=new Register();
+import data from "../../fixtures/example.json"
+
+
+
+describe("Suite-2",function()
+{
+    it("TC_02",function()
+    {
+        cy.log("TC_02 executed")
+
+    })
+})
